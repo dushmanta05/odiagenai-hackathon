eeedd950a28bc11349034b2b054e5cddc86879d4
Dushmanta <dushmanta0511@gmail.com>
2025-07-06 05:05:04 +0530

    chore: add cors to backend

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,30 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 
 import generateRouter from './routes/generate.js';
 
 const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
+const port = process.env.PORT;
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(',').map((origin) => origin.trim())
+  : [];
+app.get('/', (_, res) => {
   res.send('Hello from Express with pnpm!');
 });
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use('/generate', generateRouter);
 
